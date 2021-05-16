6643a346bfb6aa9a35994da29d113c3853003e2d
whw <whw520byl@163.com>
2021-05-16 12:22:08 +0800

    提交icon修改，正路

--- src/base/base-menu.vue
+++ src/base/base-menu.vue
@@ -2,7 +2,11 @@
     <div class="vs-menu" style="height:auto">
         <div>
             <p title="隐藏" @click="hideMenu"> <i class="iconfont icon-jiahao1"></i></p>
+            <p title="删除"><i class="iconfont icon-shanchu"></i></p>
             <p title="复制"><i class="iconfont icon-fuzhi"></i></p>
+            <p title="清除"><i class="iconfont icon-qingchu"></i></p>
+            <p title="上移一层"><i class="iconfont icon-shangyi"></i></p>
+            <p title="上移一层"><i class="iconfont icon-xiayi"></i></p>
             <slot name="imgComponent"></slot>
             <slot name="pluginComponent"></slot>
             <slot name="textComponent"></slot>
@@ -33,7 +37,7 @@ export default {
 	top: 0;
 	right: -35px;
 	overflow: hidden;
-	transition-property: height;
+	transition-property: all;
 	transition-duration: 5ms;
 	transition-timing-function: cubic-bezier(0.075, 0.82, 0.165, 1);
 	p {
